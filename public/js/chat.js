cc7e88aee19fd5e1aeb63bb3afb9800a31bf687d
bechir <bechir@accessnow.org>
2016-02-19 23:14:10 +0100

    chat is actually part of the workshop

--- public/js/chat.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-$('#chat-submit').click(function () {
-    var user = $('#chat-holder').attr('data-username');
-    socket.emit('client message', user + ' : ' + $('#message').val());
-    $('#message').val('');
-});
-socket.on('server message', function (msg) {
-    $('#messages').append($('<li>').text(msg));
-});
